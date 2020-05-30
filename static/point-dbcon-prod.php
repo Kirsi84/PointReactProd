a75cfdcc28fcd0c60d3bf2f4effec2c0e37aff36
Kirsi84 <kirsi.rouvinen@edu.omnia.fi>
2020-05-30 08:53:59 +0300

    Updated PHP files

---
+++ static/point-dbcon-prod.php
@@ -0,0 +1,7 @@
+<?php 
+    //this is the file, the data of which not transfering to github!!!!!
+    $prod_palvelin   = "";
+    $prod_kayttaja   = "";  // tämä on tietokannan käyttäjä, ei tekemäsi järjestelmän
+    $prod_salasana   = "";   
+
+?> 
