82a9da002466abe17c3af93ff7c9caa567bb8b0c
Kirsi84 <kirsi.rouvinen@edu.omnia.fi>
2020-06-01 14:50:41 +0300

    Some update.

--- point-dbcon-prod.php
+++ point-dbcon-prod.php
@@ -1,8 +1,8 @@
 <?php 
     //this is the file, not transfering to github!!!!!
-    $prod_palvelin   = "127.0.0.1:53181";
-    $prod_kayttaja   = "azure";  // tämä on tietokannan käyttäjä, ei tekemäsi järjestelmän
-    $prod_salasana   = "6#vWHD_$";   
+    $prod_palvelin   = "";
+    $prod_kayttaja   = "";  // tämä on tietokannan käyttäjä, ei tekemäsi järjestelmän
+    $prod_salasana   = "";   
 
 
     // $prod_palvelin  = "localhost";
